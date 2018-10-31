2ec06e4d73fcf6894b61af78962977fd080894af
exxe <exxe06@Marc-Terences-MacBook-Pro.local>
2018-10-31 15:34:38 +0100

    second commit

---
+++ Splits/images+names+colors.swift
@@ -0,0 +1,25 @@
+//
+//  images+names+colors.swift
+//  Splits
+//
+//  Created by exxe on 31.10.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+// Images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouquet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouquet, stand, urban]
+
+// Names
+let namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN"]
+
+// Colors
+let colorsArray = [#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1), #colorLiteral(red: 0.2549019608, green: 0.3607843137, blue: 0.6431372549, alpha: 1), #colorLiteral(red: 1, green: 0.7939705253, blue: 0, alpha: 1), #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)]
